de0277e1c1ef3cc6572cfaf59b0cbffe28a8f55f
LBwaks <obwakuvictor@gmail.com>
2023-07-31 14:17:07 +0300

    home template

--- Page/views.py
+++ Page/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from random import random
 from django.contrib.postgres.search import (SearchHeadline, SearchQuery,
                                             SearchRank, SearchVector)
 from django.db.models import Prefetch, Q
@@ -13,6 +15,7 @@ from django.template.loader import render_to_string
 from django.core.mail import BadHeaderError,EmailMultiAlternatives
 from Page.tasks import send_contact_email
 from django.db.models import Count
+from Blog.models import Blog
 # Create your views here.
 
 
@@ -26,18 +29,19 @@ class Home(TemplateView):
         # tags
         tags = Tag.objects.all()
          # featured_jobs
-        featured_jobs = Job.objects.select_related('user','category').prefetch_related('tags').filter(is_featured=True)[:6]
+        #  .filter(status="Open",deadline__gte=datetime.now())
+        featured_jobs = Job.objects.select_related('user','category').prefetch_related('tags').filter(is_featured=True,status="Open",deadline__gte=datetime.now())[:6]
         
          # recent_jobs 
-        recent_jobs = Job.objects.select_related('user','category').prefetch_related('tags').order_by('-created')[:6]
+        recent_jobs = Job.objects.select_related('user','category').prefetch_related('tags').filter(status="Open",deadline__gte=datetime.now()).order_by('-created')[:6]
         
         # popular_jobs.
-        popular_jobs = Job.objects.select_related('user','category').prefetch_related('tags').order_by('-hit_count_generic__hits')[:6]
+        popular_jobs = Job.objects.select_related('user','category').prefetch_related('tags').filter(status="Open",deadline__gte=datetime.now()).order_by('-hit_count_generic__hits')[:6]
         
         popular_tags  =Tag.objects.annotate(num_jobs=Count('job')).order_by('-num_jobs')[:5]
+        blogs = Blog.objects.select_related('user','category').order_by('-created')[:3]
         
-        
-        context={'tags':tags,'featured_jobs':featured_jobs,'recent_jobs':recent_jobs,'popular_jobs':popular_jobs,'popular_tags':popular_tags}
+        context={'blogs':blogs, 'tags':tags,'featured_jobs':featured_jobs,'recent_jobs':recent_jobs,'popular_jobs':popular_jobs,'popular_tags':popular_tags}
         
         return context
     
@@ -53,12 +57,12 @@ class JobSearchView(ListView):
 
     
     def  get_queryset(self):
-        queryset = super().get_queryset().select_related('category', 'user').prefetch_related('tags')
+        queryset = super().get_queryset().select_related('category', 'user').prefetch_related('tags').filter(status="Open",deadline__gte=datetime.now())
         tag =self.request.GET.get('tag')
         title= self.request.GET.get('title')
         location = self.request.GET.get('location')      
         
-        if tag != '0':
+        if tag and tag != '0':
             tag= get_object_or_404(Tag,pk=tag)
             queryset = queryset.filter(Q(tags__name__icontains=tag.name)|Q(tags=tag))
          
@@ -68,6 +72,7 @@ class JobSearchView(ListView):
             search_query =SearchQuery(title)
             search_headline = SearchHeadline('title',search_query)
             queryset = queryset.annotate(search=search_vector,rank=SearchRank(search_vector,search_query)).annotate(headline=search_headline).order_by('-rank')
+            
         if location:
             search_vector =(
                 SearchVector('county',weight="A")
@@ -105,30 +110,30 @@ class JobSearchView(ListView):
     
 
     
-class HomeView(ListView):
-    template_name = 'home.html'
-    context_object_name = 'jobs'
-    paginate_by = 10
+# class HomeView(ListView):
+#     template_name = 'home.html'
+#     context_object_name = 'jobs'
+#     paginate_by = 10
 
-    def get_queryset(self):
-        # Get featured jobs (assuming featured jobs have is_featured=True)
-        featured_jobs = Job.objects.filter(is_featured=True)
+#     def get_queryset(self):
+#         # Get featured jobs (assuming featured jobs have is_featured=True)
+#         featured_jobs = Job.objects.filter(is_featured=True)
 
-        # Get recent jobs (assuming recent jobs are sorted by creation date)
-        recent_jobs = Job.objects.filter(is_published=True).order_by('-created')
+#         # Get recent jobs (assuming recent jobs are sorted by creation date)
+#         recent_jobs = Job.objects.filter(is_published=True).order_by('-created')
 
-        # Get popular jobs (assuming popular jobs are sorted by positions)
-        popular_jobs = Job.objects.filter(is_published=True).order_by('-positions')
+#         # Get popular jobs (assuming popular jobs are sorted by positions)
+#         popular_jobs = Job.objects.filter(is_published=True).order_by('-positions')
 
-        # Get random 5 jobs from each category
-        random_featured_jobs = random.sample(list(featured_jobs), min(5, len(featured_jobs)))
-        random_recent_jobs = random.sample(list(recent_jobs), min(5, len(recent_jobs)))
-        random_popular_jobs = random.sample(list(popular_jobs), min(5, len(popular_jobs)))
+#         # Get random 5 jobs from each category
+#         random_featured_jobs = random.sample(list(featured_jobs), min(5, len(featured_jobs)))
+#         random_recent_jobs = random.sample(list(recent_jobs), min(5, len(recent_jobs)))
+#         random_popular_jobs = random.sample(list(popular_jobs), min(5, len(popular_jobs)))
 
-        # Combine the job lists
-        job_list = random_featured_jobs + random_recent_jobs + random_popular_jobs
+#         # Combine the job lists
+#         job_list = random_featured_jobs + random_recent_jobs + random_popular_jobs
 
-        return job_list
+#         return job_list
 
 
 class ContactView(FormView):
