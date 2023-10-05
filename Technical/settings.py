af307c0f0e20b81feaae1c1c915397d40d2a9f82
LBwaks <obwakuvictor@gmail.com>
2023-10-05 09:47:39 +0300

    dep 2

--- Technical/settings.py
+++ Technical/settings.py
@@ -37,7 +37,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG=True
 ALLOWED_HOSTS = ["*"]
